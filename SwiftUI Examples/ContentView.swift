8820a5079e37204d854b6564f2ffc1984ac34308
James Blasius <blastum@gmail.com>
2020-05-14 07:33:30 -0500

    Initial Commit

---
+++ SwiftUI Examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Examples
+//
+//  Created by James Blasius on 5/14/20.
+//  Copyright © 2020 Blastum Unlimited. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
